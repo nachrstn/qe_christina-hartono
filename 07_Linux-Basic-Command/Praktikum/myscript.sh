3c5f22d8e92b3e34261ffeacd0130a1c27e9f949
nachrstn <christinahartono@student.unud.ac.id>
2023-09-03 01:25:44 +0800

    upload file .sh

---
+++ 07_Linux-Basic-Command/Praktikum/myscript.sh
@@ -0,0 +1,16 @@
+echo "Halo, selamat datang di script punya Christina"
+
+tanggal=$(date +"%Y-%m-%d")
+echo "Tanggal hari ini adalah $tanggal"
+
+direktori=$(pwd)
+echo "Anda sedang berada di direktori : $direktori"
+
+mkdir direktorialta
+echo "Direktori direktorialta telah dibuat"
+
+mv direktorialta direktoriku
+echo "direktorialta diganti nama menjadi direktoriku"
+
+echo "direktoriku sudah muncul seperti dibawah ini"
+ls
